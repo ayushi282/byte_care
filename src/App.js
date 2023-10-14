dc7400ed7fc94dafd0754a67078aed893e2cc0ae
ayushi282 <ayushimukherjee0@gmail.com>
2023-10-15 03:37:42 +0530

    new content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import Pricing from './Component/Pricing';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './Component/Home';
-import Product from './Component/Product';
+import Product from './Component/About';
 import Resources from './Component/Resources';
 import Navbar from './Component/Navbar';
+import Blogs from './Component/Blogs';
+import FAQs from './Component/FAQs';
+import Contact from './Component/Contact';
 
 function App() {
   return (
-    <div>
+   
      <BrowserRouter>
  <Navbar/>
     <Routes>
            <Route path='Home'  element={<Home/>}/>
-      <Route path='Product'  element={<Product/>}/>
+      <Route path='About'  element={<Product/>}/>
       <Route path='Resources'  element={<Resources/>}/>
       <Route path='Pricing' Component={Pricing} element={<Pricing/>}/>
+    <Route path ='Blogs' element={<Blogs/>}/>
+    <Route path='FAQs' element={<FAQs/>}/>
+    <Route path='Contact' element={<Contact/>}/>
     </Routes>
     </BrowserRouter>
-    </div>
+    
   );
 }
 
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,30 +3,28 @@ import '../Style/Navbar.css';
 import Pricing from './Pricing';
 import { Link } from 'react-router-dom';
 import Home from './Home';
+import Blogs from './Blogs';
+import Contact from './Contact'
 
 const Navbar=()=>{
 
  return (
-<div>
-<header >
-    <div >
-       
-        <div >
-            <nav>
-            <Link to={'Home'}>Home</Link>
-                <Link  to={'Product'}>Product</Link>
-                <Link to={'Resources'}>Resources</Link>
-                <Link  to={'Pricing'}>Pricing</Link>
+   <header className="header" >
+         <nav className="nav">
+         <img className="img" src="/images/bytescarelogo.svg"></img>
+            <Link className="text" to={'Home'}>Home</Link>
+                <Link className="text" to={'About'}>About</Link>
+                <Link className="text" to={'Resources'}>Product & Services</Link>
+                <Link className="text" to={'Blogs'}>Blogs</Link>
+                <Link className="text" to={'FAQs'}>FAQs</Link>
+                <Link className="text"to={'Contact'}>Contact</Link>
                 <div>
-           <a  href='Login'>login</a> 
-            <button >Sign up</button> 
+           <a className="login" href='Login'>login</a> 
+           <Link to={'Demo'}><button className="request">Request Demo</button> </Link> 
         </div>
-            </nav>
-        </div>
-       
-    </div>
-    </header>
-</div>
+            </nav> 
+</header>
+
  );
 }
 
